77f717a6cb7e964230df6c8d6af35d08b5538ff9
Serhii Tomashkevich <19tomat@gmail.com>
2021-03-31 03:33:57 +0300

    +add:task-6

--- js/task-06.js
+++ js/task-06.js
@@ -1,1 +1,18 @@
 const inputRef = document.querySelector('#validation-input');
+
+const dataLength = inputRef.dataset.length;
+
+const onValidInput = event => {
+  const lendthValue = event.currentTarget.value.length;
+  if (dataLength > lendthValue) {
+    inputRef.classList.add('invalid');
+  } else {
+    inputRef.classList.add('valid');
+  }
+  if (inputRef.classList.contains('invalid') && inputRef.classList.contains('valid')) {
+    inputRef.classList.remove('invalid');
+    inputRef.classList.remove('valid');
+  }
+};
+
+inputRef.addEventListener('change', onValidInput);
