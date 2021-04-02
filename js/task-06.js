97b93b14245e2cfe0e8c5404251e94c172b99ef9
Serhii Tomashkevich <19tomat@gmail.com>
2021-04-02 22:17:06 +0300

    ~update:task-6 refactoring function, add getCls function

--- js/task-06.js
+++ js/task-06.js
@@ -2,8 +2,9 @@ const inputRef = document.querySelector('#validation-input');
 
 const onValidInput = event => {
   const isValidLendth = Number(inputRef.dataset.length) <= event.currentTarget.value.length;
-  const cls = isValidLendth ? 'valid' : 'invalid';
-  const inverseCls = isValidLendth ? 'invalid' : 'valid';
+  const getCls = boolean => (boolean ? 'valid' : 'invalid');
+  const cls = getCls(isValidLendth);
+  const inverseCls = getCls(!isValidLendth);
 
   inputRef.classList.contains(inverseCls) ? inputRef.classList.remove(inverseCls) : 0;
   inputRef.classList.add(cls);
